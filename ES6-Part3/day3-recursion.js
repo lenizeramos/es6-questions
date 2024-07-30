d18e43a8671a4a8f195f93a832683643513092fe
lenizeramos <lenizemr@gmail.com>
2024-07-30 13:43:32 -0700

    day3-recursion completed

--- ES6-Part3/day3-recursion.js
+++ ES6-Part3/day3-recursion.js
@@ -1,12 +1,22 @@
 //Print only items (emojis) in the console. Not the arrays.
-
-const printItems = function (array) {};
+let count = 0;
+function printItems(array) {
+  count++;
+  console.log(count);
+  for (const element of array) {
+    if (Array.isArray(element)) {
+      printItems(element);
+    } else {
+      console.log(element);
+    }
+  }
+}
 
 const array = ["😎", "💩", "🤗", "😼", "😂"];
-printItems(array);
+//printItems(array);
 
 const array2 = ["😎", ["💩", "🤗"], "😼", "😂"];
-printItems(array2);
+//printItems(array2);
 
 const array3 = [
   "😎",
