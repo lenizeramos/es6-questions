32b8317e000db1fa6ea42edbc30f2992e328d564
lenizeramos <lenizemr@gmail.com>
2024-07-30 13:14:11 -0700

    day3-spread completed

--- ES6-Part3/day3-spread-rest-operator.js
+++ ES6-Part3/day3-spread-rest-operator.js
@@ -6,7 +6,6 @@ const person2 = { age: 30, job: "Engineer" };
 
 //1. **Combine Arrays:**
 const combinedFruits = [...fruitBasket, ...additionalFruits];
-
 //console.log(`combinedFruits: ${combinedFruits}`)
 
 //2. **Clone an Array:**
@@ -21,22 +20,35 @@ const updatedStudentInfo = { ...studentInfo, grade: 100 };
 function sumNumbers(...numbers) {
   return numbers.reduce((sum, currentNumber) => sum + currentNumber, 0);
 }
+//console.log(sumNumbers(57, 8, 9, 14));
 
-console.log(sumNumbers(57, 8, 9, 14));
 //5. **Spread in Function Call:**
-//function exampleFunction() {}
+function exampleFunction(...fruit) {
+  return fruit
+}
+//console.log(exampleFunction(...fruitBasket))
 
 //6. **Merging Objects:**
-//const mergedPerson =
+const mergedPerson = {...person1, ...person2}
+//console.log(mergedPerson)
 
 //7. **Spread in Function Parameters:**
-//function mergeObjects() {}
+function mergeObjects(obj1, obj2) {
+  return {...obj1, ...obj2}
+}
+//console.log(mergeObjects(person1, person2))
 
 //8. **Destructuring and Spread:**
-//const modifiedStudentInfo =
+const {name, age, ...rest} = person1
+const modifiedStudentInfo = {name: "Leni", age: 33, ...rest}
+//console.log(modifiedStudentInfo)
 
 //9. **Rest Operator in Array:**
-//function firstElementAndRest() {}
+function firstElementAndRest([firstElement, ...rest]) {
+  return [firstElement, rest]
+}
+console.log(firstElementAndRest(fruitBasket))
 
 //10. **Spread in Object Properties:**
-//const updatedStudentInfo2 = { };
+const updatedStudentInfo2 = {...studentInfo, age:30 };
+console.log(updatedStudentInfo2)
